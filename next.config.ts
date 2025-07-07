f8c1f903dccb583baad7d1c1ecd21159bcd4c895
Amelie Braun <ameliebraun99@gmail.com>
2025-07-07 09:29:34 +0000

    Something went wrong in the App Hosting build or rollout process. Look a

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,3 @@
 // This file is intentionally left blank.
 // The configuration has been moved to next.config.js to resolve a startup issue.
-module.exports = {};
+export {};
--- src/next.config.ts
+++ src/next.config.ts
@@ -1,2 +1,2 @@
 // This file is intentionally left blank to resolve a build issue.
-module.exports = {};
+export {};
